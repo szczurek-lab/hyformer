aa45b6ac7889d73f97c69b6b2c2d5bdc1965262c
AdamIzdebski <adam.izdebski1@gmail.com>
2025-08-07 19:10:49 +0200

    Update configuration files, add new examples, and remove deprecated scripts. Introduce requirements.txt for dependencies and adjust model definitions in hyformer.

--- experiments/joint_training/assess_distribution_learning.sh
+++
@@ -1,9 +0,0 @@
-model=$1
-ckpt=$2
-
-python3 experiments/joint_training/assess_distribution_learning.py \
-        --path_to_model_ckpt ${ckpt} \
-        --path_to_model_config configs/models/${model} \
-        --path_to_tokenizer_config configs/tokenizers/smiles \
-        --chembl_training_file data/guacamol/train/smiles.txt \
-        --output outputs/${model}/assess_distribution_learning.js
--- hyformer/models/auto.py
+++ hyformer/models/auto.py
@@ -8,86 +8,26 @@ class AutoModel:
     @classmethod
     def from_config(cls, config: ModelConfig, **kwargs) -> BaseModel:
         
-        if config.model_name == 'FancyModel':
-            return getattr(importlib.import_module(
-                "hyformer.models.fancy_model"),
-                "FancyModel").from_config(config)
-        
-        elif config.model_name == 'DummyClassifierConstant':
-            return getattr(importlib.import_module(
-                "hyformer.models.dummy_classifier"),
-                "DummyClassifierConstant").from_config(config, **kwargs)
-        
-        elif config.model_name == 'FancyModelForDownstreamPrediction':
-            return getattr(importlib.import_module(
-                "hyformer.models.fancy_model"),
-                "FancyModelForDownstreamPrediction").from_config(config, **kwargs)
-        
-        elif config.model_name == 'GPT':
+        if config.model_name == 'GPT':
             return getattr(importlib.import_module(
                 "hyformer.models.gpt"),
                 "GPT").from_config(config)
-
-        elif config.model_name == 'GPTForDownstreamPrediction':
-            return getattr(importlib.import_module(
-                "hyformer.models.gpt"),
-                "GPTForDownstreamPrediction").from_config(config, **kwargs)
-        
-        elif config.model_name == 'JointGPTForDownstreamPrediction':
-            return getattr(importlib.import_module(
-                "hyformer.models.gpt"),
-                "JointGPTForDownstreamPrediction").from_config(config, **kwargs)
 
         elif config.model_name == 'Hyformer':
             return getattr(importlib.import_module(
                 "hyformer.models.hyformer"),
                 "Hyformer").from_config(config)
-        
-        elif config.model_name == 'HyformerV2':
-            return getattr(importlib.import_module(
-                "hyformer.models.hyformer_v2"),
-                "Hyformer").from_config(config)
-        
+                
         elif config.model_name == 'HyformerForDownstreamPrediction':
             return getattr(importlib.import_module(
-                "hyformer.models.hyformer_v2"),
-                "HyformerForDownstreamPrediction").from_config(config, **kwargs)
-        
-        elif config.model_name == 'HyformerLlamaForDownstreamPrediction':
-            return getattr(importlib.import_module(
                 "hyformer.models.hyformer"),
-                "HyformerForDownstreamPrediction").from_config(config, **kwargs)
-        
-        elif config.model_name == 'HyformerLlamaForDownstreamPredictionDeep':
-            return getattr(importlib.import_module(
-                "hyformer.models.hyformer"),
-                "HyformerForDownstreamPredictionDeep").from_config(config, **kwargs)
-
-        elif config.model_name == 'HyformerWithPrefix':
-            return getattr(importlib.import_module(
-                "hyformer.models.hyformer"),
-                "HyformerWithPrefix").from_config(config)
-        
-        elif config.model_name == 'HyformerWithMaxEmbeddings':
-            return getattr(importlib.import_module(
-                "hyformer.models.hyformer"),
-                "HyformerWithMaxEmbeddings").from_config(config)
-
-        elif config.model_name == 'GPT':
-            return getattr(importlib.import_module(
-                "hyformer.models.gpt"),
-                "GPT").from_config(config)
+                "HyformerForDownstreamPrediction").from_config(config)
         
         elif config.model_name == 'ChemBERTa' and config.prediction_task_type == 'classification':
             return getattr(importlib.import_module(
                 "hyformer.models.chemberta"),
                 "RobertaForSequenceClassification").from_config(config)
         
-        elif config.model_name == 'ChemBERTa' and config.prediction_task_type == 'regression':
-            return getattr(importlib.import_module(
-                "hyformer.models.chemberta"),
-                "RobertaForRegression").from_config(config)
-
         if config.model_name == "Moler":
             return getattr(importlib.import_module(
                 "hyformer.models.moler"),
@@ -107,11 +47,6 @@ class AutoModel:
             return getattr(importlib.import_module(
                 "hyformer.models.molgpt"),
                 "MolGPT").from_config(config)
-        
-        if config.model_name == "HyformerWithContext":
-            return getattr(importlib.import_module(
-                "hyformer.models.hyformer_encoder_context"),
-                "HyformerWithContext").from_config(config)
         
         else:
             raise ValueError(f"Model {config.model_name} not supported.")
