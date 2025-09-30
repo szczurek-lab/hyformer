d02a3a5d6a09f48dc732cb2334f1e176864ba303
AdamIzdebski <adam.izdebski1@gmail.com>
2025-09-30 13:47:11 +0200

    Remove migrate_guacamol.sh script to streamline the project and eliminate unused components.

--- migrate_guacamol.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-fname=$(python -c "from guacamol import frechet_benchmark; print(frechet_benchmark.__file__)")
-sed -i 's/ChemNet_v0.13_pretrained.h5/ChemNet_v0.13_pretrained.pt/g' $fname
