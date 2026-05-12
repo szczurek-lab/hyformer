112a7e3a6ff4d20f26bfefab9687644176f538f0
AdamIzdebski <adam.izdebski1@gmail.com>
2026-05-12 13:40:14 +0200

    Add warning message for Hyformer v1 regarding molecular sequences

--- hyformer/__init__.py
+++ hyformer/__init__.py
@@ -2,6 +2,14 @@
 
 __version__ = "1.0.0"
 
+import warnings
+warnings.warn(
+    "Hyformer v1: designed for molecular (SMILES) sequences."
+    "For other sequence types see Hyformer 2.0 (https://github.com/szczurek-lab/hyformer/tree/hyformer-2.0).",
+    UserWarning,
+    stacklevel=2,
+)
+
 
 from hyformer.models.auto import AutoModel
 from hyformer.models.hyformer import Hyformer
